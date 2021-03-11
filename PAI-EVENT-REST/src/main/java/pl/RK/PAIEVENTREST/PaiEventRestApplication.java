a737bd69c993fd0fbf3076b907976bc23a10f22c
Karatonik <mat.kalksztejn@wp.pl>
2021-03-11 10:57:04 +0100

    SPRING INIT

---
+++ PAI-EVENT-REST/src/main/java/pl/RK/PAIEVENTREST/PaiEventRestApplication.java
@@ -0,0 +1,13 @@
+package pl.RK.PAIEVENTREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaiEventRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaiEventRestApplication.class, args);
+	}
+
+}
---
+++ PAI-EVENT-REST/src/test/java/pl/RK/PAIEVENTREST/PaiEventRestApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.RK.PAIEVENTREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaiEventRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
