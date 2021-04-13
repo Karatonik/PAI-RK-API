40f16a9864d96c16aac33f0316e8966e80290614
Karatonik <mat.kalksztejn@wp.pl>
2021-04-13 12:20:42 +0200

    sample password

--- PAI-EVENT-REST/src/main/java/pl/RK/PAIEVENTREST/models/UserPAI.java
+++ PAI-EVENT-REST/src/main/java/pl/RK/PAIEVENTREST/models/UserPAI.java
@@ -30,6 +30,7 @@ public class UserPAI {
     //Password must contain at least one uppercase Latin character [A-Z].
     //Password must contain at least one special character like ! @ # & ( ).
     //Password must contain a length of at least 8 characters and a maximum of 20 characters.
+    //Sample : !Mocnehaslo12345
     String password;
 
     @NotBlank(message ="nick cannot be blank " )
