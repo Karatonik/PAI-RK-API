f42a11a8d9ea20cd14d8aff1d3f38c9f02d96744
Karatonik <mat.kalksztejn@wp.pl>
2021-04-13 12:49:16 +0200

    fix 2

--- PAI-EVENT-REST/src/main/java/pl/RK/PAIEVENTREST/configs/CorsConfig.java
+++ PAI-EVENT-REST/src/main/java/pl/RK/PAIEVENTREST/configs/CorsConfig.java
@@ -5,16 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-@Configuration
-@EnableWebMvc
-public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-        .allowedHeaders("*")
-        .allowedOrigins("http://localhost:3000");
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
     }
 
 
