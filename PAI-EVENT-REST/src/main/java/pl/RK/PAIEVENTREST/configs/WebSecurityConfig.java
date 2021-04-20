38a0ddcf8abcc3e9d355ab0f97df892e15c5ca2a
Karatonik <mat.kalksztejn@wp.pl>
2021-04-20 12:58:13 +0200

    2

--- PAI-EVENT-REST/src/main/java/pl/RK/PAIEVENTREST/configs/WebSecurityConfig.java
+++ PAI-EVENT-REST/src/main/java/pl/RK/PAIEVENTREST/configs/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package pl.RK.PAIEVENTREST.configs;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -70,6 +71,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                         , "/api/event/**"
                         , "/api/parti**"
                         , "api/user**").permitAll()
+                .antMatchers(HttpMethod.POST).permitAll()
                 .anyRequest().authenticated();
 
 
