703ff09af8ea2b6d06f39ddf75785795563de94b
Karatonik <mat.kalksztejn@wp.pl>
2021-05-06 12:32:02 +0200

    fix and hello controller

---
+++ src/main/java/pl/RK/PAIEVENTREST/controllers/HelloController.java
@@ -0,0 +1,20 @@
+package pl.RK.PAIEVENTREST.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@EnableSwagger2
+public class HelloController {
+
+
+
+    @GetMapping("/")
+    public String getHello(){
+        return "Hello";
+    }
+}
