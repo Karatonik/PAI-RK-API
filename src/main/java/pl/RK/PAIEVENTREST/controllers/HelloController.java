45c41ec7fcc9c54229c06f193a445bb325325fe4
Karatonik <mat.kalksztejn@wp.pl>
2021-05-06 17:19:50 +0200

    4

--- src/main/java/pl/RK/PAIEVENTREST/controllers/HelloController.java
+++ src/main/java/pl/RK/PAIEVENTREST/controllers/HelloController.java
@@ -15,6 +15,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String getHello(){
-        return "Api projekt PAI /n Authors :API-Karaton , FRONT-ROMZI ,SUPPORT-ZUZI";
+        return "Api projekt PAI | Authors :|API-Karaton|  |FRONT-ROMZI|  |SUPPORT-ZUZI|";
     }
 }
