f2450226bf6a90e7aaa0872dc7503410fd2d4191
Karatonik <mat.kalksztejn@wp.pl>
2021-05-15 23:54:46 +0200

    fix

--- src/main/java/pl/RK/PAIEVENTREST/repositorys/CommentRepository.java
+++ src/main/java/pl/RK/PAIEVENTREST/repositorys/CommentRepository.java
@@ -4,10 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.RK.PAIEVENTREST.models.Comment;
 import pl.RK.PAIEVENTREST.models.EventPAI;
+import pl.RK.PAIEVENTREST.models.UserPAI;
 
 import java.util.List;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment,Integer> {
 
     public List<Comment> findByEventPAI(EventPAI eventPAI);
+
+
+    public List<Comment> findByUserPai (UserPAI userPAI);
 }
